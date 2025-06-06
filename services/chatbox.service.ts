99c7cfb167393fad6386f41dc22c5356ad115b5b
Lê Nguyễn Chí Bảo <baolncse183721@fpt.edu.vn>
2025-06-06 18:55:35 +0700

    Replace Gemini 1.5 with Gemini 2.0-flash

--- services/chatbox.service.ts
+++ services/chatbox.service.ts
@@ -48,7 +48,7 @@ export interface SendMessageResponse {
 
 const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || 'http://192.168.1.35:3000'
 const GEMINI_API_KEY = process.env.EXPO_PUBLIC_GEMINI_API_KEY
-const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent';
+const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
 
 export const fetchUserChatHistory = async (userId: string): Promise<ChatHistoryResponse | { error: string }> => {
   try {
